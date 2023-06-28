3934065e542c7fb7756bd70076ed5892a2760a21
Сергей Горохов <s.gorokhov91@gmail.com>
2023-06-28 12:24:55 +0300

    Create dao for book and create method for first book page

---
+++ src/main/java/ru/sergey/libraryproject/dao/BookDAO.java
@@ -0,0 +1,23 @@
+package ru.sergey.libraryproject.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.sergey.libraryproject.model.Book;
+
+import java.util.List;
+
+@Component
+public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM book",new BeanPropertyRowMapper<>(Book.class));
+    }
+}
