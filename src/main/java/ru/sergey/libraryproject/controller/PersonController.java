05ddb27e78afd185ab85ee2ea7c5fa59ca046de4
Сергей Горохов <s.gorokhov91@gmail.com>
2023-06-29 11:27:12 +0300

    add index page and dao logic for people

---
+++ src/main/java/ru/sergey/libraryproject/controller/PersonController.java
@@ -0,0 +1,30 @@
+package ru.sergey.libraryproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sergey.libraryproject.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PersonController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("peopleList", personDAO.index());
+        return "people/index";
+    }
+
+
+
+
+}
---
+++ src/main/java/ru/sergey/libraryproject/dao/PersonDAO.java
@@ -0,0 +1,24 @@
+package ru.sergey.libraryproject.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.sergey.libraryproject.model.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
