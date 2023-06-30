807f022d87d4152e1ed319dffa70239f1f54fb7c
Сергей Горохов <s.gorokhov91@gmail.com>
2023-06-30 20:25:49 +0300

    add simple main page

---
+++ src/main/java/ru/sergey/libraryproject/controller/MainController.java
@@ -0,0 +1,12 @@
+package ru.sergey.libraryproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String mainPage(){
+        return "main";
+    }
+}
