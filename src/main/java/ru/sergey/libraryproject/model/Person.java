402dea7efe3f4c4a6463649bb4c382462f80768d
Сергей Горохов <s.gorokhov91@gmail.com>
2023-07-11 13:46:51 +0300

    update Person model for work with Hibernate and change name of id field

--- src/main/java/ru/sergey/libraryproject/model/Person.java
+++ src/main/java/ru/sergey/libraryproject/model/Person.java
@@ -1,21 +1,34 @@
 package ru.sergey.libraryproject.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
+@Entity
+@Table(name = "person")
 public class Person {
 
-    private int person_id;
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @NotEmpty(message = "Name should not be empty")
+    @Column(name = "fullname")
     private String fullName;
 
     @Min(value = 1900,message = "Birth year cant be less than 1900")
     @Max(value = 2023,message = "Birth year cant be bigger than 2023")
     @NotNull(message = "birth year cant be empty")
+    @Column(name="birthyear")
     private Integer birthYear;
+
+    @OneToMany(mappedBy = "person")
+    private List<Book> books;
 
     public Person() {
     }
@@ -26,12 +39,20 @@ public class Person {
 
     }
 
-    public int getPerson_id() {
-        return person_id;
+    public int getId() {
+        return id;
     }
 
-    public void setPerson_id(int person_id) {
-        this.person_id = person_id;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 
     public String getFullName() {
