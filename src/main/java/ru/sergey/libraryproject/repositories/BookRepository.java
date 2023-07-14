e23a48a4d3e166a74fd7f3c7ce8623c2e82dd66d
Сергей Горохов <s.gorokhov91@gmail.com>
2023-07-14 17:42:04 +0300

    add query signatures in book repo

--- src/main/java/ru/sergey/libraryproject/repositories/BookRepository.java
+++ src/main/java/ru/sergey/libraryproject/repositories/BookRepository.java
@@ -1,9 +1,23 @@
 package ru.sergey.libraryproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.sergey.libraryproject.models.Book;
+import ru.sergey.libraryproject.models.Person;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findBooksByOwnerId(int id);
+
+    @Modifying
+    @Query("UPDATE Book b SET b.owner=?1 WHERE b.id=?2")
+    void attachPersonToBook(Person person,int bookId);
+
+    @Modifying
+    @Query("UPDATE Book b SET b.owner=null WHERE b.id=?1")
+    void unbindPersonFromBook(int bookId);
 }
